457a57d83c45264e123f3535a6c8c92ee8a69d9d
Lucas Rodrigues <lucas.lcs93@hotmail.com>
2021-02-15 21:44:16 -0300

    Course

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES('Bootcamp html', 'https://cdn.pixabay.com/photo/2018/03/22/10/55/training-course-3250007_1280.jpg', 'https://cdn4.iconfinder.com/data/icons/e-learning-1-4/66/59-512.png');
+INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES('Bootcamp Java', 'https://cdn.pixabay.com/photo/2018/03/22/10/55/training-course-3250007_1280.jpg', 'https://cdn4.iconfinder.com/data/icons/e-learning-1-4/66/59-512.png');
